4a5d2626da3ce3e3b95f9dce0492f4515ac8abd0
amj210 <ooaa1738@gmail.com>
2023-11-05 20:14:48 +0200

    fixeddd upload

--- lib/features/feature_auth/presentation/screens/sign_up_screen.dart
+++ lib/features/feature_auth/presentation/screens/sign_up_screen.dart
@@ -10,11 +10,11 @@ class SignUpScreen extends StatelessWidget {
   SignUpScreen({Key? key}) : super(key: key);
   static const String route = "SignUpScreen";
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:SafeArea(child:  SingleChildScrollView(
+      body: SafeArea(
+          child: SingleChildScrollView(
         child: Center(
             child: ConstrainedBox(
                 constraints: BoxConstraints(
@@ -30,119 +30,116 @@ class SignUpScreen extends StatelessWidget {
                     ),
                     Form(
                         child: SizedBox(
-                          width: 389.w,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                'Email Address',
-                                style: TextStyle(
-                                  color: ExternalColors.gray,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              Padding(
-                                  padding:
-                                  const EdgeInsets.only(bottom: 22, top: 9)
-                                      .h,
-                                  child: SizedBox(
-                                    height: 48,
-                                    child:  TextFormField(
-                                            decoration: const InputDecoration(
-                                                border: OutlineInputBorder(),
-                                                hintText: "****@gmail.com")),
-                                  )),
-                              const Text(
-                                'Password',
-                                style: TextStyle(
-                                  color: ExternalColors.gray,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              Padding(
-                                padding:
-                                const EdgeInsets.only(bottom: 22, top: 9).h,
-                                child: SizedBox(
-                                  height: 48,
-                                  child: TextFormField(
+                      width: 389.w,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Email Address',
+                            style: TextStyle(
+                              color: ExternalColors.gray,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Padding(
+                              padding:
+                                  const EdgeInsets.only(bottom: 22, top: 9).h,
+                              child: SizedBox(
+                                height: 48,
+                                child: TextFormField(
                                     decoration: const InputDecoration(
                                         border: OutlineInputBorder(),
-                                        hintText: "********"),
+                                        hintText: "****@gmail.com")),
+                              )),
+                          const Text(
+                            'Password',
+                            style: TextStyle(
+                              color: ExternalColors.gray,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(bottom: 22, top: 9).h,
+                            child: SizedBox(
+                              height: 48,
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: "********"),
+                              ),
+                            ),
+                          ),
+                          const Text(
+                            'Date of Birth',
+                            style: TextStyle(
+                              color: ExternalColors.gray,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Padding(
+                              padding:
+                                  const EdgeInsets.only(bottom: 22, top: 9).h,
+                              child: SizedBox(
+                                height: 48,
+                                child: TextFormField(
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
                                   ),
                                 ),
-                              ),
-                              const Text(
-                                'Date of Birth',
-                                style: TextStyle(
-                                  color: ExternalColors.gray,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              Padding(
-                                  padding:
-                                  const EdgeInsets.only(bottom: 22, top: 9)
-                                      .h,
-                                  child: SizedBox(
-                                    height: 48,
-                                    child: TextFormField(
-                                      decoration: const InputDecoration(
-                                        border: OutlineInputBorder(),
-                                      ),
-                                    ),
-                                  )),
-                              SizedBox(
-                                width: 389.w,
-                                height: 48,
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.of(context)
-                                        .pushReplacementNamed(HomeScreen.route);
-                                  },
-                                  style: ButtonStyle(
-                                      backgroundColor:
+                              )),
+                          SizedBox(
+                            width: 389.w,
+                            height: 48,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pushReplacementNamed(HomeScreen.route);
+                              },
+                              style: ButtonStyle(
+                                  backgroundColor:
                                       MaterialStateProperty.all<Color>(
                                           ExternalColors.yellow)),
-                                  child: const Text("signUp"),
-                                ),
-                              ),
-                              Padding(
-                                padding:
-                                const EdgeInsets.symmetric(vertical: 22).h,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(' have an account? ',
-                                        style: TextStyle(
-                                          color: const Color(0xFF323A46),
-                                          fontSize: 13,
-                                          fontWeight: FontWeight.w500,
-                                          height: 0.14.h,
-                                        )),
-                                    GestureDetector(
-                                        onTap: () {},
-                                        child: Text(
-                                          ' login',
-                                          style: TextStyle(
-                                            color: const Color(0xFF3568FF),
-                                            fontSize: 13,
-                                            fontWeight: FontWeight.w500,
-                                            height: 0.14.h,
-                                          ),
-                                        ))
-                                  ],
-                                ),
-                              ),
-                              const OrWidget(),
-                              SizedBox(
-                                height: 13.h,
-                              ),
-                              const OtherSignUp()
-                            ],
+                              child: const Text("signUp"),
+                            ),
                           ),
-                        ))
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 22).h,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(' have an account? ',
+                                    style: TextStyle(
+                                      color: const Color(0xFF323A46),
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w500,
+                                      height: 0.14.h,
+                                    )),
+                                GestureDetector(
+                                    onTap: () {},
+                                    child: Text(
+                                      ' login',
+                                      style: TextStyle(
+                                        color: const Color(0xFF3568FF),
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.w500,
+                                        height: 0.14.h,
+                                      ),
+                                    ))
+                              ],
+                            ),
+                          ),
+                          const OrWidget(),
+                          SizedBox(
+                            height: 13.h,
+                          ),
+                          const OtherSignUp()
+                        ],
+                      ),
+                    ))
                   ],
                 ))),
       )),
